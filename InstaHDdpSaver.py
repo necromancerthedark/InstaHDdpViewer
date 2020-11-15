e85e594a26eadd532ac532c5f0af6101b3e5193d
Kumar Gaurav Pandey <47948736+necromancerthedark@users.noreply.github.com>
2020-11-15 21:20:44 +0530

    added multiple photo at once

--- InstaHDdpSaver.py
+++ InstaHDdpSaver.py
@@ -3,17 +3,20 @@ from PIL import Image
 import requests
 from io import BytesIO
 import getpass
+import json
 
-username = input("Enter Your Username: ")
-password = getpass.getpass('Password: ')
-api = Client(username, password)
+secret = open("secret.json", "r")
+details = json.loads(secret.read())
+api = Client(details['username'], details['password'])
 results = api.feed_timeline()
 
-target_user_id = input('Enter User Id: ')
-file_name = target_user_id + '.jpg'
-details = api.user_detail_info(target_user_id)
-imdataurl = details['user_detail']['user']['hd_profile_pic_url_info']['url']
-image = requests.get(imdataurl)
-img = Image.open(BytesIO(image.content))
-img.show()
-img.save(file_name)
+names = open('people.txt', 'r')
+target_user_id = names.readline().strip()
+while target_user_id != "":
+    file_name = target_user_id + '.jpg'
+    details = api.user_detail_info(target_user_id)
+    imdataurl = details['user_detail']['user']['hd_profile_pic_url_info']['url']
+    image = requests.get(imdataurl)
+    img = Image.open(BytesIO(image.content))
+    img.save(file_name)
+    target_user_id = names.readline().strip()
